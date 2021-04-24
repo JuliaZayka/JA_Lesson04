861e56950ef56b02ef60609c394c87ba24836666
julia <julia.zayka18@gmail.com>
2021-04-24 11:45:49 +0300

    Init

---
+++ scr/main/java/ua/lviv/Registrationservlet.java
@@ -0,0 +1,34 @@
+package ua.lviv;
+
+import java.io.IOException; 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@SuppressWarnings("unused")
+public class Registrationservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UserService userService = UserService.getUserService();   
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	response.getWriter().append("Served at: ").append(request.getContextPath());
+  }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		//System.out.println(email);
+	userService.saveUser(new User(firstName, lastName, email, password));
+	request.setAttribute("userEmail", email);		
+	request.getRequestDispatcher("cabinet.jsp").forward(request, response);
+	}
+
+}
